5b5d7d934be05e670ceb9bf9c15de02b1f57ff6d
David <2908113940@qq.com>
2020-12-06 19:55:08 +0800

    增加gallery app, called “myApp1"

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from myapp1.models import Gallery
+
 
 def home(request):
-    return render(request,'home.html')
+    gallerys = Gallery.objects   # 把Gallery的所有对象都取过来
+    return render(request,'home.html',{"gallerys": gallerys})
 
 def new(request):
     return HttpResponse('你好')
